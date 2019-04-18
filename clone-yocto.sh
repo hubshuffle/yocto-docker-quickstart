9f07136a83296cc9338472033345bdf83caf989b
hubshuffle <hubshuffled@gmail.com>
2019-04-18 20:49:28 +0100

    Make clone-yocto.sh smarter

--- clone-yocto.sh
+++ clone-yocto.sh
@@ -2,9 +2,28 @@
 
 set -e
 
-mkdir -p workspace/yocto/layers && cd workspace/yocto
-git clone -b thud git://git.yoctoproject.org/poky.git
-cd layers
-git clone -b thud https://github.com/agherzan/meta-raspberrypi.git
-git clone -b thud https://github.com/openembedded/meta-openembedded.git
-cd ../../
+usage() {
+  echo "Usage: $0 <target-directory>"
+}
+
+if [ $# -ne 1 ]; then
+  usage
+  exit 1
+fi
+
+directory=$1
+
+if [ -e "${directory}" ]; then
+  echo "Error: \"${directory}\" already exists."
+  usage
+fi
+
+# create the directory
+mkdir -p "${directory}/sources" && cd "${directory}"
+
+# clone poky and other layers
+git clone -b thud git://git.yoctoproject.org/poky.git sources/poky
+git clone -b thud https://github.com/agherzan/meta-raspberrypi.git sources/meta-raspberrypi
+git clone -b thud https://github.com/openembedded/meta-openembedded.git sources/meta-openembedded
+
+echo "Done, type \"cd ${directory} && . ./sources/poky/oe-init-build-env\" to create the build environment"
